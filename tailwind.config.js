fbfe616dfd3ef04a0d8b0a830daaf13e79c10e4a
lauxlorenzo <lorenzolaux05@gmail.com>
2024-05-14 11:48:06 -0300

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    colors: {
+      'blue': '#0D65D9',
+      'lg-blue': '#94BDF2',
+      'yellow': '#F5D20F',
+      'green': '#00D639',
+    },
+    extend: {},
+  },
+  plugins: [],
+}
+
